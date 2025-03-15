d07748beb1310534191ce8b0b1eb8a866b6509b0
Pranav Rajendraprasad <pranavprasad.r500@gmail.com>
2025-03-15 17:13:40 +0530

    setimage

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Navbar from './Homepage/Navbar';
+import Main1 from './Homepage/Main1';
 
 export default function Homepage() {
     return(
-        <Navbar/>
+        <>
+            <Navbar/>
+            <Main1/>
+        </>
     );
 }
--- frontend/src/components/Homepage/Main1.jsx
+++ frontend/src/components/Homepage/Main1.jsx
@@ -1,7 +1,11 @@
-export default function() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+export default function Main1() {
     return(
         <>
-            
+            <img className='container-fluid p-0 m-0 h-170'  src="./Gorge.jpeg" alt="Image" />
         </>
     );
 }
